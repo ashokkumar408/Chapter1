147d14bafb1b2190b7e61116d3815a8bb4c3320d
ashokkumar408 <ashokkumar.poornakumar@trumpf.com>
2025-01-10 16:15:35 +0530

    main: Added methods

--- project1/Exercise_1.3.c
+++ project1/Exercise_1.3.c
@@ -1,8 +1,8 @@
 // Exercise_1.3: Modify the temparature conversion program to print a heading above the table.
 #include <stdio.h>
 
-main(){
-	printf("fahrenheit value is:         celsius value is:\n");
+Main1_3(){
+	printf("Fahrenheit         Celsius\n");
 	int fahr, celsius, lower = 0, upper = 300, step = 20;
 	fahr = lower;
 	while (fahr <= upper) {
